a9d0bd5c0eb89053fdd186a0b31bb0a20681505e
vishal <vishalsenthilkumaran05@gmail.com>
2016-07-17 22:03:48 +0530

    Create cv.java

---
+++ cv.java
@@ -0,0 +1,23 @@
+
+import java.util.*;
+public class even
+{
+public static void main(String[] args)
+{
+Scanner sc=new Scanner(System.in)
+int e=sc.nextInt();
+while ( e <= 100 )
+    {
+
+     e = e +1;
+
+     if ( e % 2 == 0)
+     {
+
+     System.out.print(" " + e);
+     }
+
+  }
+}
+}
+
