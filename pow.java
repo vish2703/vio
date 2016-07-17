53e5801809f018d39aed189640333fc8847fb847
vishal <vishalsenthilkumaran05@gmail.com>
2016-07-17 15:25:24 +0530

    Update and rename vi.java to pow.java

---
+++ pow.java
@@ -0,0 +1,14 @@
+
+import java.util.*;
+
+public class FindPowerExample {
+ 
+  public static void main(String[] args) {
+   
+   
+     System.out.println(Math.pow(2,2));
+   
+     
+     System.out.println(Math.pow(-3,2));
+  }
+}
